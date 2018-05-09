f49f58c43b27a7a1a539af6a0a9364535cc7aa11
Andrew Mattie <andrewmattie@mattieapps.com>
2018-05-09 12:16:18 -0400

    Follow Signal style guide

--- src/org/thoughtcrime/securesms/service/LinkingService.java
+++ src/org/thoughtcrime/securesms/service/LinkingService.java
@@ -46,6 +46,17 @@ public class LinkingService extends Service {
   public static final String LINKING_EVENT = "org.thoughtcrime.securesms.LINKING_EVENT";
   public static final String LINKING_PUBKEY = "org.thoughtcrime.securesms.LINKING_PUBKEY";
   private String deviceNameExtra;
+  private String password;
+  private String temporarySignalingKey;
+  private SignalServiceAccountManager accountManager;
+  private IdentityKeyPair temporaryIdentity;
+  private Intent intent;
+  private SignalServiceAccountManager.NewDeviceRegistrationReturn ret;
+  private int registrationId;
+  private String gcmRegistrationId;
+  private GroupSyncRequestJob groupSyncRequestJob;
+  private IdentityKeyPair retIdentity;
+  private String username;
 
   @Override
   public IBinder onBind(Intent intent) {
@@ -68,58 +79,12 @@ public class LinkingService extends Service {
     try {
       deviceNameExtra = linkIntent.getStringExtra("device_name");
 
-      /* create tsdevice link */
-      String password = Util.getSecret(18);
-      IdentityKeyPair temporaryIdentity = KeyHelper.generateIdentityKeyPair();
-      SignalServiceAccountManager accountManager = new SignalServiceAccountManager(new SignalServiceNetworkAccess(this).getConfiguration(this), null, password, BuildConfig.USER_AGENT);
+      createTsDeviceLink();
+      finishLink();
+      saveIdentity();
+      createPreKeys();
+      savePublicKeyToDB();
 
-      String uuid = accountManager.getNewDeviceUuid(); /* timeouts sometimes */
-      URI tsdevicelink = new URI("tsdevice:/?uuid=" + URLEncoder.encode(uuid, "utf-8") + "&pub_key=" + URLEncoder.encode(Base64.encodeBytesWithoutPadding(temporaryIdentity.getPublicKey().serialize()), "utf-8"));
-      Log.d("LinkingService", tsdevicelink.toString());
-      Intent intent = new Intent(LINKING_PUBKEY);
-      intent.putExtra(LINKING_PUBKEY, tsdevicelink.toString());
-      LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-
-      /* finish link */
-      String temporarySignalingKey = Util.getSecret(52);
-      int registrationId = KeyHelper.generateRegistrationId(false);
-      String deviceName = deviceNameExtra;
-      SignalServiceAccountManager.NewDeviceRegistrationReturn ret = accountManager.finishNewDeviceRegistration(temporaryIdentity, temporarySignalingKey, false, true, registrationId, deviceName);
-      String gcmRegistrationId = GoogleCloudMessaging.getInstance(this).register(GcmRefreshJob.REGISTRATION_ID);
-      accountManager.setGcmId(Optional.of(gcmRegistrationId));
-
-      GroupSyncRequestJob groupSyncRequestJob = new GroupSyncRequestJob(this);
-
-      /* save identity and deviceid */
-      TextSecurePreferences.setDeviceId(this, ret.getDeviceId());
-      final String username = ret.getNumber();
-      TextSecurePreferences.setLocalNumber(this, ret.getNumber());
-      final IdentityKeyPair retIdentity = ret.getIdentity();
-      IdentityKeyUtil.setIdentityKeys(this, retIdentity);
-
-      /* create PreKeys */
-      List<PreKeyRecord> oneTimePreKeys = PreKeyUtil.generatePreKeys(this);
-      SignedPreKeyRecord signedPreKey = PreKeyUtil.generateSignedPreKey(this, retIdentity, true);
-      accountManager.setPreKeys(retIdentity.getPublicKey(), signedPreKey, oneTimePreKeys);
-
-      /* save own public key to DB */
-      Recipient self = Recipient.from(this, Address.fromExternal(this, username), false);
-      DatabaseFactory.getIdentityDatabase(this).saveIdentity(self.getAddress(), ret.getIdentity().getPublicKey(), IdentityDatabase.VerifiedStatus.VERIFIED, true, 0, true);
-
-      TextSecurePreferences.setVerifying(this, false);
-      TextSecurePreferences.setPushRegistered(this, true);
-      TextSecurePreferences.setPushServerPassword(this, password);
-      TextSecurePreferences.setSignalingKey(this, temporarySignalingKey);
-      TextSecurePreferences.setSignedPreKeyRegistered(this, true);
-      TextSecurePreferences.setPromptedPushRegistration(this, true);
-      TextSecurePreferences.setWebsocketRegistered(this, true);
-      TextSecurePreferences.setGcmRegistrationId(this, gcmRegistrationId);
-      TextSecurePreferences.setMultiDevice(this, true);
-      TextSecurePreferences.setLocalRegistrationId(this, registrationId);
-      DirectoryRefreshListener.schedule(this);
-      RotateSignedPreKeyListener.schedule(this);
-
-      /* send sync groups */
       ApplicationContext.getInstance(this).getJobManager().add(groupSyncRequestJob);
 
       intent = new Intent(LINKING_EVENT);
@@ -127,6 +92,74 @@ public class LinkingService extends Service {
     } catch (Exception e) {
       Log.w("LinkingService", e);
     }
+  }
+
+  private void createTsDeviceLink() {
+    try {
+      password = Util.getSecret(18);
+      temporaryIdentity = KeyHelper.generateIdentityKeyPair();
+      accountManager = new SignalServiceAccountManager(new SignalServiceNetworkAccess(this).getConfiguration(this), null, password, BuildConfig.USER_AGENT);
+
+      String uuid = accountManager.getNewDeviceUuid(); /* timeouts sometimes */
+      URI tsdevicelink = new URI("tsdevice:/?uuid=" + URLEncoder.encode(uuid, "utf-8") + "&pub_key=" + URLEncoder.encode(Base64.encodeBytesWithoutPadding(temporaryIdentity.getPublicKey().serialize()), "utf-8"));
+      Log.d("LinkingService", tsdevicelink.toString());
+      intent = new Intent(LINKING_PUBKEY);
+      intent.putExtra(LINKING_PUBKEY, tsdevicelink.toString());
+      LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    } catch(Exception e) {
+      Log.w("LinkingService", e);
+    }
+  }
+
+  private void finishLink() {
+    try {
+      temporarySignalingKey = Util.getSecret(52);
+      registrationId = KeyHelper.generateRegistrationId(false);
+      String deviceName = deviceNameExtra;
+      ret = accountManager.finishNewDeviceRegistration(temporaryIdentity, temporarySignalingKey, false, true, registrationId, deviceName);
+      gcmRegistrationId = GoogleCloudMessaging.getInstance(this).register(GcmRefreshJob.REGISTRATION_ID);
+      accountManager.setGcmId(Optional.of(gcmRegistrationId));
+
+      groupSyncRequestJob = new GroupSyncRequestJob(this);
+    } catch (Exception e) {
+      Log.w("LinkingService", e);
+    }
+  }
+
+  private void saveIdentity() {
+    TextSecurePreferences.setDeviceId(this, ret.getDeviceId());
+    username = ret.getNumber();
+    TextSecurePreferences.setLocalNumber(this, ret.getNumber());
+    retIdentity = ret.getIdentity();
+    IdentityKeyUtil.setIdentityKeys(this, retIdentity);
+  }
+
+  private void createPreKeys() {
+    try {
+      List<PreKeyRecord> oneTimePreKeys = PreKeyUtil.generatePreKeys(this);
+      SignedPreKeyRecord signedPreKey = PreKeyUtil.generateSignedPreKey(this, retIdentity, true);
+      accountManager.setPreKeys(retIdentity.getPublicKey(), signedPreKey, oneTimePreKeys);
+    } catch(Exception e) {
+      Log.w("LinkingService", e);
+    }
+  }
+
+  private void savePublicKeyToDB() {
+    Recipient self = Recipient.from(this, Address.fromExternal(this, username), false);
+    DatabaseFactory.getIdentityDatabase(this).saveIdentity(self.getAddress(), ret.getIdentity().getPublicKey(), IdentityDatabase.VerifiedStatus.VERIFIED, true, 0, true);
+
+    TextSecurePreferences.setVerifying(this, false);
+    TextSecurePreferences.setPushRegistered(this, true);
+    TextSecurePreferences.setPushServerPassword(this, password);
+    TextSecurePreferences.setSignalingKey(this, temporarySignalingKey);
+    TextSecurePreferences.setSignedPreKeyRegistered(this, true);
+    TextSecurePreferences.setPromptedPushRegistration(this, true);
+    TextSecurePreferences.setWebsocketRegistered(this, true);
+    TextSecurePreferences.setGcmRegistrationId(this, gcmRegistrationId);
+    TextSecurePreferences.setMultiDevice(this, true);
+    TextSecurePreferences.setLocalRegistrationId(this, registrationId);
+    DirectoryRefreshListener.schedule(this);
+    RotateSignedPreKeyListener.schedule(this);
   }
 
   @Override
